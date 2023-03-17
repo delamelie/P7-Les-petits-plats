ba526a0f4b5d799c06e6df9d1e80d135a103a026
Amélie de Lima <amelie.delima@hotmail.fr>
2023-03-17 13:27:20 +0100

    Fix files' path

--- scripts/search-tags.js
+++ scripts/search-tags.js
@@ -1,4 +1,4 @@
-import { removeAccents, search } from "./search-function.js"
+import { removeAccents, search } from "../search-function.js"
 
 
 //////////////////////////////////////////// DOM elements /////////////////////////////////////////////////////
